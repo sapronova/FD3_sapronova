2be1dc16f469285ca74f1a730439e1ec702157d0
sapronova <64229064+sapronova@users.noreply.github.com>
2021-02-19 00:42:19 +0300

    HT ishop2 first commitment

---
+++ ishop2/prodComp.js
@@ -0,0 +1,35 @@
+var ProdComp = React.createClass({
+
+    displayName: 'ProdComp',
+    
+    propTypes: {
+        item: React.PropTypes.number.isRequired,
+        name: React.PropTypes.string.isRequired,
+        price: React.PropTypes.string.isRequired,
+        url: React.PropTypes.string.isRequired,
+        count: React.PropTypes.number.isRequired,
+        cbSelected: React.PropTypes.func.isRequired,
+        cbDelete: React.PropTypes.func.isRequired,
+    },
+
+    highlighted: function (EO) { 
+        this.props.cbSelected (this.props.item);
+    },
+
+    delItem: function (EO) {
+        this.props.cbDelete (this.props.index);
+    },
+
+    render: function() {
+      return React.DOM.tr ( {className:this.props.classname, onClick:this.highlighted},
+        React.DOM.td({},
+            React.DOM.img({className:'Image', src:  this.props.url, alt:'image' }),),
+            React.DOM.td({className:'Text'},  this.props.name),
+            React.DOM.td({className:'Price'},  this.props.price),
+            React.DOM.td({className:'Count'},  this.props.count, ' pcs.'),
+            React.DOM.td({className:'Delete'}, 
+            React.DOM.input ({type:'button', value:'Delete', onClick:this.delItem})),
+    );
+    },
+    
+    });
---
+++ ishop2/shopComp.js
@@ -0,0 +1,63 @@
+var ShopComp = React.createClass({
+    displayName: 'ProdComp',
+    
+    getDefaultProps: function() {
+      return { shop: "Undefined" }
+    },
+    
+    propTypes: {
+        positions: React.PropTypes.array.isRequired,
+    },
+
+    getInitialState: function () {
+        return {
+            isHighlightened: null,
+            selectedItem:null,
+            myProds: this.props.positions.map(v=>v),
+        };
+    },
+
+    itemSelected: function(item) {
+        this.setState({selectedItem: item});
+        this.setState({isHighlightened: 'Highlightened'});
+        this.state.isHighlightened==='Highlightened'&&item===this.state.selectedItem?this.setState({isHighlightened: null}):this.setState({isHighlightened: 'Highlightened'});
+    },
+
+    itemDelete: function(index) { 
+        var confirmation = confirm('You have clicked the delete-button, are you sure of your decision?');
+        if (confirmation) { 
+            this.state.myProds.splice(index,1);
+        };
+    },
+
+    render: function() {
+        var prodList=this.state.myProds.map ((v, i) =>
+            React.createElement (ProdComp, {key:v.item, item:v.item, name:v.name, count:v.count, 
+                price:v.price, url:v.url, 
+                cbSelected: this.itemSelected, 
+                classname:(v.item===this.state.selectedItem)?this.state.isHighlightened:null,
+                cbDelete: this.itemDelete, 
+                index: i,
+            }
+            ) 
+        );
+    
+        return React.DOM.div({className: 'ItemsList'},
+        React.DOM.div({className: 'Name'}, this.props.shop),
+        React.DOM.table({className: 'ItemsList'},
+            React.DOM.thead(null,
+                React.DOM.tr({className: 'ListHeader'},
+                    React.DOM.th(null, 'Image'),
+                    React.DOM.th({className: 'Text'}, 'Name'), 
+                    React.DOM.th({className: 'Price'}, 'Price'),
+                    React.DOM.th({className: 'Count'}, 'Count'),
+                    React.DOM.th({className: 'Delete'}, 'Delete'),
+                )
+            ),
+                React.DOM.tbody({className: 'List'}, prodList)
+        ),
+
+    );
+ 
+    },
+});
