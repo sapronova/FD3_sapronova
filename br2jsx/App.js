31912c86caad7f43514ad1a139b39e9c49bfd0d3
sapronova <64229064+sapronova@users.noreply.github.com>
2021-03-11 23:41:43 +0300

    v1.0.0

---
+++ br2jsx/App.js
@@ -0,0 +1,13 @@
+"use strict";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import BR2JSX from './components/BR2JSX';
+
+let text="первый<br>второй<br/>третий<br />последний";
+
+ReactDOM.render(
+  <BR2JSX text={text}/>,
+  document.getElementById('cont4brtojsx')
+)
---
+++ br2jsx/components/BR2JSX.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './BR2JSX.css'; 
+
+const BR2JSX = props => {
+    let modifiedText=props.text.split(/<br *\/?>/);
+    let modifiedTexttoJSX=[];
+    for (const [i, v] of modifiedText.entries()) {  
+      modifiedTexttoJSX.push(v);
+      modifiedTexttoJSX.push(<br key={i}/>);
+        };
+ 
+    return <div>{modifiedTexttoJSX}</div>;
+  }
+  
+  BR2JSX.propTypes = {
+    text: PropTypes.string.isRequired,
+  };
+  
+  export default BR2JSX; 
+
+  /*let modifiedTexttoJSX=[];
+      modifiedText.forEach ((v,i)=>{   
+          modifiedTexttoJSX.push(v);
+          modifiedTexttoJSX.push(<br key={i}/>);
+        });
+        */
